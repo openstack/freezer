272df481089ccea9bf2688146bdb1c43f9d4ca60
Zahari Zahariev <zahari.ven.zahariev@hp.com>
2015-04-27 15:58:35 +0100

    Fix broken functional tests freezer_main()
    Functional tests need to pass arguments directly to freezer_main()
    as a regular dictionary.
    
    Change-Id: I4ded66723ccb5f65d6f00415a8030917ce3c84ea
    Implements: blueprint functional-testing

--- freezer/main.py
+++ freezer/main.py
@@ -29,7 +29,6 @@ import subprocess
 import logging
 import sys
 
-
 # Initialize backup options
 (backup_args, arg_parse) = backup_arguments()
 
@@ -40,68 +39,72 @@ logging.basicConfig(
     format='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 
-def configure_log_file_using_defaults():
-    """ Configure log file for freezer """
-
-    dry_run_message = ''
-    if backup_args.dry_run:
-        dry_run_message = '[DRY_RUN] '
-
-    def configure_logging(file_name):
-        expanded_file_name = os.path.expanduser(file_name)
-        expanded_dir_name = os.path.dirname(expanded_file_name)
-        create_dir(expanded_dir_name, do_log=False)
-        logging.basicConfig(
-            filename=expanded_file_name,
-            level=logging.INFO,
-            format=('%(asctime)s %(name)s %(levelname)s {0}%(message)s'.
-                    format(dry_run_message)))
-        return expanded_file_name
-
-    if backup_args.log_file:
-        return configure_logging(backup_args.log_file)
-
-    for file_name in ['/var/log/freezer.log', '~/.freezer/freezer.log']:
-        try:
-            return configure_logging(file_name)
-        except IOError:
-            pass
-
-    raise Exception("Unable to write to log file")
-
-
-def set_max_process_priority():
-    """ Set freezer in max priority on the os """
-    # children processes inherit niceness from father
-    try:
-        logging.warning(
-            '[*] Setting freezer execution with high CPU and I/O priority')
-        PID = os.getpid()
-        # Set cpu priority
-        os.nice(-19)
-        # Set I/O Priority to Real Time class with level 0
-        subprocess.call(
-            [u'{0}'.format(backup_args.ionice),
-                u'-c', u'1', u'-n', u'0', u'-t', u'-p', u'{0}'.format(PID)])
-    except Exception as priority_error:
-        logging.warning('[*] Priority: {0}'.format(priority_error))
-
-
-def fail(exit_code, e, do_log=True):
-    """ Catch the exceptions and write it to log """
-    msg = '[*] Critical Error: {0}\n'.format(e)
-    if not backup_args.quiet:
-        sys.stderr.write(msg)
-    if do_log:
-        logging.critical(msg)
-    sys.exit(exit_code)
-
-
-def freezer_main():
+def freezer_main(args={}):
     """Freezer main loop for job execution.
     """
+    global backup_args, arg_parse
 
-    if len(sys.argv) < 2:
+    def configure_log_file_using_defaults():
+        """ Configure log file for freezer """
+
+        dry_run_message = ''
+        if backup_args.dry_run:
+            dry_run_message = '[DRY_RUN] '
+
+        def configure_logging(file_name):
+            expanded_file_name = os.path.expanduser(file_name)
+            expanded_dir_name = os.path.dirname(expanded_file_name)
+            create_dir(expanded_dir_name, do_log=False)
+            logging.basicConfig(
+                filename=expanded_file_name,
+                level=logging.INFO,
+                format=('%(asctime)s %(name)s %(levelname)s {0}%(message)s'.
+                        format(dry_run_message)))
+            return expanded_file_name
+
+        if backup_args.log_file:
+            return configure_logging(backup_args.log_file)
+
+        for file_name in ['/var/log/freezer.log', '~/.freezer/freezer.log']:
+            try:
+                return configure_logging(file_name)
+            except IOError:
+                pass
+
+        raise Exception("Unable to write to log file")
+
+    def set_max_process_priority():
+        """ Set freezer in max priority on the os """
+        # children processes inherit niceness from father
+        try:
+            logging.warning(
+                '[*] Setting freezer execution with high CPU and I/O priority')
+            PID = os.getpid()
+            # Set cpu priority
+            os.nice(-19)
+            # Set I/O Priority to Real Time class with level 0
+            subprocess.call([
+                u'{0}'.format(backup_args.ionice),
+                u'-c', u'1', u'-n', u'0', u'-t',
+                u'-p', u'{0}'.format(PID)
+                ])
+        except Exception as priority_error:
+            logging.warning('[*] Priority: {0}'.format(priority_error))
+
+    def fail(exit_code, e, do_log=True):
+        """ Catch the exceptions and write it to log """
+        msg = '[*] Critical Error: {0}\n'.format(e)
+        if not backup_args.quiet:
+            sys.stderr.write(msg)
+        if do_log:
+            logging.critical(msg)
+        sys.exit(exit_code)
+
+    # Alternative aruments provision useful to run Freezer without
+    # command line e.g. functional testing
+    if args:
+        backup_args.__dict__.update(args)
+    elif len(sys.argv) < 2:
         arg_parse.print_help()
         sys.exit(1)
 
@@ -115,7 +118,7 @@ def freezer_main():
         fail(1, err, do_log=False)
 
     if not backup_args.quiet:
-        print 'log file at {0}'.format(log_file_name)
+        logging.info('log file at {0}'.format(log_file_name))
 
     if backup_args.max_priority:
         set_max_process_priority()
