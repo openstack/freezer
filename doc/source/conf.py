5956339ccaaece3e8ad88b3897bdd1159de48dfc
Daniel Mellado <dmellado@redhat.com>
2016-02-08 17:26:42 +0100

    Start using oslosphinx theme for docs
    Closes-Bug: #1542061
    Change-Id: I99edd066e6860298a3e7bdc26b245eae6ec68d96

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,10 +28,9 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'oslosphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
