9f8d6c382e5c33528faf6893d4cb7973d19a64e2
David Rabel <rabel@b1-systems.de>
2018-05-03 18:49:36 +0200

    Fix license boilerplates
    license boilerplates as docstring are included in automatically
    generated documentation which leads to wrong formatting.
    
    For example the indented hyperlink to the license is formatted as
    quotation.
    
    Change-Id: I9d6088ab48d6787f29b593bfa5445b6a84e99de7

--- freezer/lib/pep3143daemon/__init__.py
+++ freezer/lib/pep3143daemon/__init__.py
@@ -1,11 +1,13 @@
 # flake8: noqa
+#
+# Copyright (c) 2014, Stephan Schultchen.
+#
+# License: MIT (see LICENSE for details)
+
+
 """
 pep3143daemon is a implementation of the PEP 3143, describing a well behaving
 Unix daemon, as documented in Stevens 'Unix Network Programming'
-
-Copyright (c) 2014, Stephan Schultchen.
-
-License: MIT (see LICENSE for details)
 """
 
 from freezer.lib.pep3143daemon.daemon import DaemonContext, DaemonError
