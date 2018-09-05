openstack/freezer

commit 50df2c8ca3e1083ea53a0937a83a35bb9ed1d499

Fix backup nova instance with creating job error
Currently, it will fail when doing nova backup with creating
a backup job. It is because freezer-scheduler will call freezer-agent
with '--config config-file', but the config opts in config-file (from
job file) were not parsed to CONF, at the same time EngineManager uses
the CONF.engine_name(now it is the default 'tar'), this will lead the
code go into wrong branch, although it will not throw errors and show
the backup is success, but when doing restore after this, we can find
that the backup data can not be used and print EOF error in log file.

This patch will fix the above issue.

Change-Id: I826e4ed71dfd2cde6103e8432faba7adb4648c36
Closes-Bug: #1710238

freezer/common/config.py
@@ -20,6 +20,7 @@ import sys
 import tempfile
 
 from oslo_config import cfg
+from oslo_config.cfg import NoSuchOptError
 from oslo_log import log
 from oslo_utils import encodeutils
 
@@ -550,6 +551,12 @@ def get_backup_args():
         defaults['log_config_append'] = None
 
         defaults.update(conf.default)
+        for config_key in conf.default.keys():
+            try:
+                CONF.set_override(config_key, conf.default[config_key])
+            except NoSuchOptError:
+                LOG.debug('No such opt, {0}, so set it'.format(config_key))
+                setattr(CONF, config_key, conf.default[config_key])
 
         if defaults['log_file']:
             CONF.set_override('log_file', defaults['log_file'])

commit afbbaa2326d5e51ded7b06f7b1257e1a7807c152

Fixed using 'os_auth_ver' parameter when passed via --config
Although using 'os_auth_ver' parameter is deprecated, freezer-api (or
freezer-web-ui respectively) still uses it to generate configs for jobs.
Commit 50df2c8 introduced regression causing freezer-agent refusing to
start when config passed via '--config' contains 'os_auth_ver' parameter
(or any other deprecated option). The root cause is that loop (L568)
iterates through keys defined in config file passed via '--config'
therefore trying to call CONF.set_override with deprecated 'os_auth_ver'
as a first argument which causes oslo.config library to raise
KeyError: 'os-identity-api-version' as CONF.set_override is not expected
to be called with deprecated options. This simple patch adds call for
get for each key which may (correctly) raise NoSuchOptError for undefined
(and deprecated) keys, thus safely fallbacking to except routine.

Re-submitted.

Change-Id: Ia554d440b6256eab0f953ed9ad75cff1f040e6a1

freezer/common/config.py
@@ -566,6 +566,7 @@ def get_backup_args():
         defaults.update(conf.default)
         for config_key in conf.default.keys():
             try:
+                CONF.get(config_key)
                 CONF.set_override(config_key, conf.default[config_key])
             except NoSuchOptError:
                 LOG.debug('No such opt, {0}, so set it'.format(config_key))